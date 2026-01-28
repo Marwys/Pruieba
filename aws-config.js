2b8e835cd705ed1a6f1681ef529716a59a0f1677
Marwys <114246393+Marwys@users.noreply.github.com>
2026-01-28 13:53:47 -0500

    Update aws-config.js
    Cambiar a ID de invitados

--- aws-config.js
+++ aws-config.js
@@ -1,5 +1,5 @@
 window.awsConfig = {
   region: "us-east-1",
-  identityPoolId: "us-east-1:deb2b8bb-088e-40bb-97d2-209d1bddd27a",
+  identityPoolId: "us-east-1:7860e2f2-eb64-4494-9c2a-fe735c8912ec",
   iotEndpoint: "a1eyoxsww0xvbl-ats.iot.us-east-1.amazonaws.com"
 };
