9ea838e79587b7b4e45f635b2fad4b4af08fb362
Marwys <114246393+Marwys@users.noreply.github.com>
2026-01-28 13:49:04 -0500

    Create aws-config.js

---
+++ aws-config.js
@@ -0,0 +1,5 @@
+window.awsConfig = {
+  region: "us-east-1",
+  identityPoolId: "us-east-1:deb2b8bb-088e-40bb-97d2-209d1bddd27a",
+  iotEndpoint: "a1eyoxsww0xvbl-ats.iot.us-east-1.amazonaws.com"
+};
